4a134a5f41f8b3ba1ba0cbb91e217e38eba89f0c
Zekerocks011 <59491890+Zekerocks011@users.noreply.github.com>
2021-06-18 14:06:24 -0400

    Update vanish.js

--- commands/vanish.js
+++ commands/vanish.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'vanish',
 	description: 'Vanishes a user',
+	aliases: ['poof'],
 	execute(message, args) {
         message.delete()
         victim = message.mentions.users.first()
