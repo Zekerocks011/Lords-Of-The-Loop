803c1ab20e3dd294647d1a04d80418a23e93a707
Zekerocks011 <59491890+Zekerocks011@users.noreply.github.com>
2021-06-18 14:05:14 -0400

    Update killed-chat.js

--- commands/killed-chat.js
+++ commands/killed-chat.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'killed-chat',
 	description: 'Makes fun of person that killed chat',
+	aliases: ['kc'],
 	execute(message, args) {
         message.delete()
         victim = message.mentions.users.first()
