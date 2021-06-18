8aaa4776b3ce1eebe83ff2aaa1fc973afc80ca9b
Zekerocks011 <59491890+Zekerocks011@users.noreply.github.com>
2021-06-18 10:58:08 -0400

    Create missingperms.js

---
+++ commands/missingperms.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'missingperms',
+	description: 'Pings Em to notify of missing perms',
+	execute(message, args) {
+        message.delete()
+        message.channel.send('<@471030262218752006> https://media.discordapp.net/attachments/850360326717571075/852932884976107560/unknown.png?width=526&height=33')
+	},
+};
