ba3ae14dbb2c06963a1b3ab501ac61e7b1a41858
Zekerocks011 <59491890+Zekerocks011@users.noreply.github.com>
2021-06-18 13:41:41 -0400

    Update characterdatabase.js

--- clientcommands/characterdatabase.js
+++ clientcommands/characterdatabase.js
@@ -55,16 +55,29 @@ module.exports = (client, Discord, SQLite, sql) =>{
     client.on("message", message=>{
         if(message.author.bot) return
         if(message.guild){
-            if(message.content === "!characters"){
-                const mes = sql.prepare(`SELECT * FROM characters ORDER BY id ASC LIMIT 20`).all()
+            if(message.content.includes("!characters")){
+                var turn = message.content.split(" ")
+                var num = parseInt(turn[1])
+                if(!num){
+                    num = 1
+                    page = 0
+                } else {
+                    var page = num*10-10
+                }
+                const mes = sql.prepare(`SELECT * FROM characters ORDER BY id ASC`).all()
                 const embed = new Discord.MessageEmbed()
                 .setTitle("List of Characters")
-                .setFooter("Page 1 of 1")
                 .setColor(0x00AE86)
       
                 for(const data of mes) {
-                    embed.addFields({ name: data.id, value: data.description })
-                } return message.channel.send({embed})
+                    var i,j,temparray,chunk = 10
+                    for(i=page,j=mes.length; i<j; i+=chunk){
+                        embed.setFooter(`Page ${num} of ${Math.ceil(mes.length/10)}`)
+                        temparray = mes.slice(i,i+chunk)
+                        temparray.forEach((item)=> embed.addFields({ name: item.id, value: item.description }))
+                        return message.channel.send({embed})
+                    }
+                }
             }
         }
     })
